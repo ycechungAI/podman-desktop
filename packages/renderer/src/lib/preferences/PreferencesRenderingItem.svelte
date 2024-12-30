fcaba8460b2cbdb2310f5b59c2aa0fe22353bc53
Florent Benoit <fbenoit@redhat.com>
2024-12-30 16:52:23 +0100

    chore: update PreferencesRenderingItem to svelte5 runes
    related to https://github.com/podman-desktop/podman-desktop/issues/10240
    Signed-off-by: Florent Benoit <fbenoit@redhat.com>

--- packages/renderer/src/lib/preferences/PreferencesRenderingItem.svelte
+++ packages/renderer/src/lib/preferences/PreferencesRenderingItem.svelte
@@ -6,15 +6,13 @@ import type { IConfigurationPropertyRecordedSchema } from '../../../../main/src/
 import Markdown from '../markdown/Markdown.svelte';
 import PreferencesRenderingItemFormat from './PreferencesRenderingItemFormat.svelte';
 
-export let record: IConfigurationPropertyRecordedSchema;
+interface Props {
+  record: IConfigurationPropertyRecordedSchema;
+}
 
-let recordUI: {
-  title: string;
-  breadCrumb: string;
-  description?: string;
-  markdownDescription?: string;
-  original: IConfigurationPropertyRecordedSchema;
-};
+const { record }: Props = $props();
+let showResetButton = $state(false);
+let resetToDefault = $state(false);
 
 // add space from camel case and upper case on the first letter
 function startCase(str: string): string {
@@ -22,7 +20,7 @@ function startCase(str: string): string {
     return str.toUpperCase();
   });
 }
-function update(record: IConfigurationPropertyRecordedSchema) {
+const recordUI = $derived.by(() => {
   const id = record.id;
   // take string after the last dot
   const key = id?.substring(id?.lastIndexOf('.') + 1) ?? '';
@@ -32,17 +30,19 @@ function update(record: IConfigurationPropertyRecordedSchema) {
   // and replace dot by > in breadcrumb
   const breadCrumbUI = breadCrumb.replace(/\./g, ' > ').concat(':');
 
-  recordUI = {
+  return {
     title: startCase(key),
     breadCrumb: breadCrumbUI,
     description: record.description,
     markdownDescription: record.markdownDescription,
     original: record,
   };
-}
-$: {
-  update(record);
-}
+});
+
+$effect(() => {
+  showResetButton = false;
+  resetToDefault = false;
+});
 
 function updateResetButtonVisibility(recordValue: any) {
   showResetButton =
@@ -56,9 +56,6 @@ function updateResetButtonVisibility(recordValue: any) {
 function doResetToDefault() {
   resetToDefault = true;
 }
-
-$: showResetButton = false;
-$: resetToDefault = false;
 </script>
 
 <div class="flex flex-row px-2 py-2 justify-between w-full text-[color:var(--pd-invert-content-card-text)]">
