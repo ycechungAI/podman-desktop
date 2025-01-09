5ef447d680e8f51616dab61d89495153057e9473
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-01-09 16:00:22 +0100

    test(ui): adding unit tests for TerminalWindows (#10531)
    * test(ui): adding unit tests for TerminalWindows
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: replace define property by vi#spy
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: simplify the tests by using prototype
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

---
+++ packages/renderer/src/lib/ui/TerminalWindows.spec.ts
@@ -0,0 +1,151 @@
+/**********************************************************************
+ * Copyright (C) 2025 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ***********************************************************************/
+
+import '@testing-library/jest-dom/vitest';
+
+import { render } from '@testing-library/svelte';
+import { FitAddon } from '@xterm/addon-fit';
+import { Terminal } from '@xterm/xterm';
+import { writable } from 'svelte/store';
+import { afterEach, beforeEach, expect, test, vi } from 'vitest';
+
+import TerminalWindow from '/@/lib/ui/TerminalWindow.svelte';
+
+import { TerminalSettings } from '../../../../main/src/plugin/terminal-settings';
+
+vi.mock('@xterm/xterm');
+
+vi.mock('@xterm/addon-fit');
+
+beforeEach(() => {
+  vi.resetAllMocks();
+});
+
+afterEach(() => {
+  vi.restoreAllMocks();
+});
+
+test('expect terminal constructor to have been called on mount', async () => {
+  render(TerminalWindow, {
+    terminal: writable() as unknown as Terminal,
+  });
+
+  await vi.waitFor(() => {
+    expect(Terminal).toHaveBeenCalledOnce();
+  });
+});
+
+test('expect terminal constructor to reflect props', async () => {
+  render(TerminalWindow, {
+    terminal: writable() as unknown as Terminal,
+    disableStdin: true,
+    convertEol: true,
+    screenReaderMode: true,
+  });
+
+  await vi.waitFor(() => {
+    expect(Terminal).toHaveBeenCalledWith(
+      expect.objectContaining({
+        disableStdin: true,
+        convertEol: true,
+        screenReaderMode: true,
+      }),
+    );
+  });
+});
+
+test('showCursor false or undefined should write specific instruction to terminal', async () => {
+  render(TerminalWindow, {
+    terminal: writable() as unknown as Terminal,
+    showCursor: false,
+  });
+
+  await vi.waitFor(() => {
+    expect(Terminal).toHaveBeenCalledOnce();
+  });
+  expect(Terminal.prototype.write).toHaveBeenCalledWith('\x1b[?25l');
+});
+
+test('terminal constructor should contains fontSize and lineHeight from configuration', async () => {
+  vi.mocked(window.getConfigurationValue).mockResolvedValue(10);
+
+  render(TerminalWindow, {
+    terminal: writable() as unknown as Terminal,
+  });
+
+  await vi.waitFor(() => {
+    expect(Terminal).toHaveBeenCalledWith(
+      expect.objectContaining({
+        fontSize: 10,
+        lineHeight: 10,
+      }),
+    );
+  });
+
+  expect(window.getConfigurationValue).toHaveBeenCalledWith(
+    TerminalSettings.SectionName + '.' + TerminalSettings.FontSize,
+  );
+  expect(window.getConfigurationValue).toHaveBeenCalledWith(
+    TerminalSettings.SectionName + '.' + TerminalSettings.LineHeight,
+  );
+});
+
+test('addon fit should be loaded on mount', async () => {
+  const fitAddonMock = {
+    fit: vi.fn(),
+  } as unknown as FitAddon;
+
+  vi.mocked(FitAddon).mockReturnValue(fitAddonMock);
+
+  render(TerminalWindow, {
+    terminal: writable() as unknown as Terminal,
+  });
+
+  await vi.waitFor(() => {
+    expect(Terminal).toHaveBeenCalled();
+  });
+
+  expect(FitAddon).toHaveBeenCalled();
+  expect(Terminal.prototype.loadAddon).toHaveBeenCalledWith(fitAddonMock);
+});
+
+test('matchMedia resize listener should trigger fit addon', async () => {
+  // spy the event listener
+  vi.spyOn(window, 'addEventListener');
+
+  render(TerminalWindow, {
+    terminal: writable() as unknown as Terminal,
+  });
+
+  const listener: () => void = await vi.waitFor(() => {
+    expect(window.addEventListener).toHaveBeenCalled();
+    const call = vi.mocked(window.addEventListener).mock.calls;
+    expect(call).toHaveLength(1);
+    expect(call[0][0]).toBe('resize');
+    expect(call[0][1]).toBeInstanceOf(Function);
+    return call[0][1] as unknown as () => void;
+  });
+
+  // reset fit calls count
+  vi.mocked(FitAddon.prototype.fit).mockReset();
+  expect(FitAddon.prototype.fit).not.toHaveBeenCalled();
+
+  listener();
+
+  expect(FitAddon.prototype.fit).toHaveBeenCalled();
+});
