7fa5f93fd3f394a87ccb09c36c25dfa1407415a2
benoitf <fbenoit@redhat.com>
2025-02-14 08:32:27 +0000

    chore: Update chocolatey packages for 1.16.2
    Signed-off-by: benoitf <fbenoit@redhat.com>

--- .chocolatey/podman-desktop/tools/chocolateyInstall.ps1
+++ .chocolatey/podman-desktop/tools/chocolateyInstall.ps1
@@ -5,9 +5,9 @@ $packageArgs = @{
   fileType       = 'exe'
   softwareName   = 'PodmanDesktop'
 
-  url64bit       = 'https://github.com/podman-desktop/podman-desktop/releases/download/v1.16.1/podman-desktop-1.16.1-setup.exe'
+  url64bit       = 'https://github.com/podman-desktop/podman-desktop/releases/download/v1.16.2/podman-desktop-1.16.2-setup.exe'
   checksumType   = 'sha256'
-  checksum64     = 'c35c10f4b98382af23a9702b09af84e19ecbce0aa3e4116559b5c3d94c5e924e'
+  checksum64     = '6799a69fd6fa88432ee09b0140571e14038351e1a15dece4a02416c73c87afc4'
 
   silentArgs     = '/S'
   validExitCodes = @(0)
